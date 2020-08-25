cc65a1700e3c94528962c9156d672a4e9aea3bb1
nutthawut <nsittee@gmail.com>
2020-08-25 23:46:49 +0700

    Change router for thread to handle the new model

--- express-back-end/src/routes/thread.js
+++ express-back-end/src/routes/thread.js
@@ -5,7 +5,8 @@ const checkAuth = require('../middleware/check-auth')
 
 router.get('/', (req, res, next) => {
   Thread.find()
-    .populate('threadPoster', 'userName')
+    .populate('Author', 'Username')
+    .populate('SubParent', ['SubLongName', 'SubShortName'])
     .exec()
     .then(threads => res.status(200).json(threads));
 });
@@ -13,28 +14,29 @@ router.get('/', (req, res, next) => {
 router.get('/:id', (req, res, next) => {
   const id = req.params.id;
   Thread.findOne()
-    .populate('threadPoster', 'userName')
+    .populate('Author', 'Username')
+    .populate('SubParent', ['SubLongName', 'SubShortName'])
     .where('_id', id)
     .exec()
     .then(thread => res.status(200).json(thread));
 });
 
-router.post('/', checkAuth, (req, res, next) => {
-  const newThread = {
-    threadTitle: req.body.threadTitle
-  }
-  res.status(200).json({
-    message: "post => thread",
-    body: newThread
-  });
-});
+// router.post('/', checkAuth, (req, res, next) => {
+//   const newThread = {
+//     threadTitle: req.body.threadTitle
+//   }
+//   res.status(200).json({
+//     message: "post => thread",
+//     body: newThread
+//   });
+// });
 
-router.delete('/:id', (req, res, next) => {
-  const id = req.params.id;
-  res.status(200).json({
-    message: "delete => thread",
-    id: id
-  });
-});
+// router.delete('/:id', (req, res, next) => {
+//   const id = req.params.id;
+//   res.status(200).json({
+//     message: "delete => thread",
+//     id: id
+//   });
+// });
 
 module.exports = router;
