af055171349876f96ebeda40db3c86b2f7f19240
nutthawut <nsittee@gmail.com>
2020-09-01 22:42:24 +0700

    Fix routing for Sub to return a similar structure and nested populate()

--- express-back-end/src/routes/sub.js
+++ express-back-end/src/routes/sub.js
@@ -4,29 +4,36 @@ const router = express.Router();
 const SubModel = require('../models/sub');
 const ThreadModel = require('../models/thread');
 
-router.get('/home', (req, res, next) => {
+router.get('/', (req, res, next) => {
   ThreadModel.find()
     .populate('Author', 'Username')
     .populate('SubParent', ['SubLongName', 'SubShortName'])
     .exec()
-    .then(threads => res.status(200).json(threads));
+    .then(threads => res.status(200).json({
+      message: "get all thread",
+      data: {
+        SubThread: threads
+      }
+    }));
 });
 
 router.get('/:name', (req, res) => {
   const subName = req.params.name;
   SubModel.findOne()
     .where({ SubLongName: subName })
-    .populate('SubThread', [
-      'Title',
-      'Content',
-      'Upvote',
-      'Downvote',
-      'CreatedDate',
-      'Author'
-    ])
+    .populate({
+      path: 'SubThread',
+      populate: [{
+        path: 'Author',
+        select: 'Username'
+      }, {
+        path: 'SubParent',
+        select: ['SubLongName', 'SubShortName']
+      }]
+    })
     .exec()
     .then(sub => {
-      res.send({
+      res.status(200).send({
         message: 'get thread for sub completed',
         data: sub
       });
