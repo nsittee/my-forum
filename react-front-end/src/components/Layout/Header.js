84a6f2f1f8a075aba5eff0f30e0e7bb6e0b65480
nutthawut <nsittee@gmail.com>
2020-09-09 00:08:43 +0700

    Add placeholder button for the sign in/out

--- react-front-end/src/components/Layout/Header.js
+++ react-front-end/src/components/Layout/Header.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import { Grid } from '@material-ui/core';
+import { Grid, Button } from '@material-ui/core';
 
 import Logo from './logo.png';
 
@@ -19,10 +18,21 @@ const Header = () => {
           </a>
           <a href="/profile">Profile</a>
           <a href="/setting">Setting</a>
+          <Button onClick={signInDialog}>
+            Sign In </Button>
+          <Button onClick={signUpDialog}>
+            Sign Up </Button>
         </Grid>
       </Toolbar>
     </AppBar>
   );
+}
+
+function signInDialog() {
+  console.log("Display dialog");
+}
+function signUpDialog() {
+  console.log("Display dialog");
 }
 
 export default Header;
