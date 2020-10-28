2509d4409bef0d28713b000a6d0cbfb4efc60012
nutthawut <nsittee@gmail.com>
2020-10-28 23:56:27 +0700

    Adjust submit page

--- react-front-end/src/components/Page/SubmitPage.js
+++ react-front-end/src/components/Page/SubmitPage.js
@@ -1,25 +1,42 @@
-import { Grid, InputLabel, MenuItem, Select } from '@material-ui/core';
+import { Button, Card, Grid, InputLabel, MenuItem, Select } from '@material-ui/core';
 import React, { useState } from 'react'
 
 const SubmitPage = () => {
-  const [community, setCommunity] = useState('Sub 45')
+  const [community, setCommunity] = useState()
   const handleChange = (event) => {
     console.log('hlp')
     setCommunity(event.target.value)
   }
 
   return (
-    <Grid>
-      <h1>Submit</h1>
-      <InputLabel>Select Community</InputLabel>
-      <Select
-        value={community}
-        onChange={event => { handleChange(event) }}
-        fullWidth={true}>
-        <MenuItem value={'Sub 45'}>Sub 45</MenuItem>
-        <MenuItem value={'Sub 100'}>Sub 100</MenuItem>
-        <MenuItem value={'Sub 620'}>Sub 620</MenuItem>
-      </Select>
+    <Grid container spacing={1} direction="column">
+      <Grid item>
+        <Card>
+          <h1>Submit</h1>
+        </Card>
+      </Grid>
+      <Grid item>
+        <Card>
+          <InputLabel>Select Community</InputLabel>
+          <Select
+            value={community}
+            onChange={event => { handleChange(event) }}
+            fullWidth={true}>
+            <MenuItem value={'Sub 45'}>Sub 45</MenuItem>
+            <MenuItem value={'Sub 100'}>Sub 100</MenuItem>
+            <MenuItem value={'Sub 620'}>Sub 620</MenuItem>
+          </Select>
+        </Card>
+      </Grid>
+      <Grid item>
+        <Card>
+          <h5>Input control</h5>
+
+          <Button variant="contained" color="secondary">
+            Submit
+          </Button>
+        </Card>
+      </Grid>
     </Grid>
   )
 }
