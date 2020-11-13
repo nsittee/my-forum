3c1a3ae4c3ddf2565500218e7b5281ef755449d3
nutthawut-itmx <nutthawut.sit@itmx.co.th>
2020-11-13 11:33:33 +0700

    Fix <MyForum/> component and fix context for ts

--- react-front-end/src/context/auth-context.ts
+++ react-front-end/src/context/auth-context.ts
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const authContext = React.createContext({
-  id: null,
-  username: null,
+  id: '',
+  username: '',
   authenticated: false,
   token: '',
   header: {}
