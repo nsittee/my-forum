84ceb44ca8aeba8b0b7d25851f8a7c1cce998737
nutthawut <nutthawut@email>
2021-07-13 00:40:25 +0700

    Update express application to serve build directory

--- src/app.js
+++ src/app.js
@@ -7,11 +7,15 @@ require('./configs/database');
 
 app.use('/api', require('./routes'));
 
-require('./middleware/error-handler')
+require('./middleware/error-handler');
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  if (process.NODE_ENV == "production") {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  } else {
+    res.send('Api is running');
+  }
 });
 
 module.exports = app;
