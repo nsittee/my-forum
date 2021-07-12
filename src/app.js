b218d1b2a25818f906183d6fe35d3cf69ed184c0
nutthawut <nutthawut@email>
2021-07-13 00:05:32 +0700

    Refactor client project

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ require('./middleware/error-handler')
 
 app.use(express.static('public'))
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 module.exports = app;
