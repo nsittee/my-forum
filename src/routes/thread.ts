a896d88ef298912c8fb452f02993a67a16364015
bon <nsittee@gmail.com>
2021-08-23 23:36:07 +0700

    Add logic for complicate vote

--- src/routes/thread.ts
+++ src/routes/thread.ts
@@ -6,6 +6,7 @@ import User from '../models/user'
 import Sub from '../models/sub'
 
 import { authenticate } from '../middleware/authenticate'
+import _ from 'lodash'
 
 const router = express.Router();
 
@@ -59,19 +60,50 @@ router.get('/vote/:id/:vote', authenticate(), async (req, res) => {
   const threadId = req.params.id
   const userVote = req.params.vote
   if (userVote !== 'up' && userVote !== 'down') return res.send('invalid')
-
   const user = await User.findOne().where('_id', userId).exec()
-  if (user.UpvoteThread.includes(threadId) || user.DownvoteThread.includes(threadId))
-    return res.send('invalid')
-  if (userVote === 'up') user.UpvoteThread = user.UpvoteThread.concat([threadId])
-  else user.DownvoteThread = user.DownvoteThread.concat([threadId])
 
   const thread = await Thread.findOne().where('_id').equals(threadId).exec()
-  if (userVote === 'up') thread.Upvote = thread.Upvote + 1
-  else thread.Downvote = thread.Downvote + 1
+  if (userVote === 'up') {
+    if (user.UpvoteThread.includes(threadId)) {
+      console.log('1 > 0')
+      thread.Upvote = thread.Upvote - 1
+      user.UpvoteThread = _.remove(user.UpvoteThread, (id: string) => id === threadId)
+    }
+    else if (user.DownvoteThread.includes(threadId)) {
+      console.log('-1 > 1')
+      thread.Downvote = thread.Downvote - 1
+      thread.Upvote = thread.Upvote + 1
+      user.UpvoteThread = user.UpvoteThread.concat([threadId])
+      user.DownvoteThread = _.remove(user.DownvoteThread, (id: string) => id === threadId)
+    }
+    else {
+      console.log('0 > 1')
+      thread.Upvote = thread.Upvote + 1
+      user.UpvoteThread = user.UpvoteThread.concat([threadId])
+    }
+  }
+  else {
+    if (user.DownvoteThread.includes(threadId)) {
+      console.log('-1 > 0')
+      thread.Downvote = thread.Downvote - 1
+      user.DownvoteThread = _.remove(user.DownvoteThread, (id: string) => id === threadId)
+    }
+    else if (user.UpvoteThread.includes(threadId)) {
+      console.log('1 > -1')
+      thread.Downvote = thread.Downvote + 1
+      thread.Upvote = thread.Upvote - 1
+      user.DownvoteThread = user.DownvoteThread.concat([threadId])
+      user.UpvoteThread = _.remove(user.UpvoteThread, (id: string) => id === threadId)
+    }
+    else {
+      console.log('0 > -1')
+      thread.Downvote = thread.Downvote + 1
+      user.DownvoteThread = user.DownvoteThread.concat([threadId])
+    }
+  }
 
-  console.log(thread);
-
+  console.log(thread.Upvote);
+  console.log(thread.Downvote);
   user.save()
   thread.save()
 
