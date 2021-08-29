9448141f56d663946372abf25e275fd542c810eb
bon <nsittee@gmail.com>
2021-08-29 20:24:24 +0700

    Fix import

--- src/client/src/page/ChangelogPage.tsx
+++ src/client/src/page/ChangelogPage.tsx
@@ -1,4 +1,4 @@
-import { Card, Container, Grid } from '@material-ui/core';
+import { Container, Grid } from '@material-ui/core';
 import React from 'react'
 import { MyCard } from '../components/common/MyCard';
 
