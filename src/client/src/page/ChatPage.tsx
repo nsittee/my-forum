554690fb45f9e1a1e08ad7758856414445c99bd9
bon <nsittee@gmail.com>
2021-09-01 18:41:24 +0700

    Add simple socket.io connection and cors

--- src/client/src/page/ChatPage.tsx
+++ src/client/src/page/ChatPage.tsx
@@ -1,12 +1,18 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container } from '@material-ui/core'
 import { MyCard } from '../components/common/MyCard'
 
+import { io } from 'socket.io-client'
 import { MyButton } from '../components/common/MyButton'
 import { MyTextField } from '../components/common/MyTextField'
 
 const ChatPage = () => {
   const [message, setMessage] = useState<string>('')
+  const socket = io('http://localhost:8080')  // TODO: Dynamic address for socket.io
+
+  useEffect(() => {
+    socket.connect()
+  }, [socket])
 
   return (
     <Container maxWidth="md">
@@ -22,7 +28,7 @@ const ChatPage = () => {
         />
         <MyButton onClick={() => {
           console.log(`send => ${message}`)
-          // socket.send(message)
+          socket.send(message)
           setMessage('')
         }}>
           Send
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import './configs/environment'
 import http from 'http'
 import app from './app'
+import { Server } from 'socket.io'
 
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  }
+})
+io.on('connection', (socket) => {
+  console.log('############## a user connected ###############')
+})
 
 server.listen(port, () => {
   console.log(`Server is running on ${port}`);
