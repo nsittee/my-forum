dd9b7619d3d0df5d7e77b91cfbcbecae6da018ff
bon <nsittee@gmail.com>
2021-09-01 19:19:57 +0700

    Add message feed to page

--- src/client/src/page/ChatPage.tsx
+++ src/client/src/page/ChatPage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Container } from '@material-ui/core'
+import { Card, Container, Paper } from '@material-ui/core'
 import { MyCard } from '../components/common/MyCard'
 
 import { io } from 'socket.io-client'
@@ -9,10 +9,11 @@ import { MyTextField } from '../components/common/MyTextField'
 const socket = io('http://localhost:8080')  // TODO: Dynamic address for socket.io
 const ChatPage = () => {
   const [message, setMessage] = useState<string>('')
+  const [chatFeed, setChatFeed] = useState<JSX.Element[]>([])
 
   socket.on('message', recMessage => {
-    console.log(`got message: ${recMessage}`);
-
+    console.log(`recMessage: ${recMessage}`);
+    setChatFeed([...chatFeed, <Card key={chatFeed.length}>{recMessage}</Card>])
   })
   useEffect(() => {
     socket.connect()
@@ -24,11 +25,14 @@ const ChatPage = () => {
       <MyCard header="Chat" />
       <br />
       <MyCard>
+        {chatFeed}
+      </MyCard>
+      <br />
+      <MyCard>
         <MyTextField
           label="Message"
           value={message}
           onChange={(event: any) => setMessage(event.target.value)}
-
         />
         <MyButton onClick={() => {
           console.log(`send => ${message}`)
