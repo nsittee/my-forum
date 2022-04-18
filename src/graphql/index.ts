7881d3f3b9564708d97d2101cd6464d05e48db48
bonbon.gaming <nsittee@gmail.com>
2022-04-19 00:52:58 +0700

    Update getThreads to match previous implementation

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -2,13 +2,14 @@ import { authenticate } from './../middleware/authenticate'
 import { graphqlHTTP } from "express-graphql"
 import { schemas } from "./schema"
 import { resolvers } from './resolver'
+import { Router } from 'express'
 
 // Setup context for req/res from express
 // https://stackoverflow.com/questions/51779230/parameters-ordering-to-get-context-in-express-graphql
 // https://stackoverflow.com/questions/48630023/wrong-order-of-graphql-resolver-arguments-root-args-context
 
 // For simplicity with auth middleware, all route will require authenticate optional for now
-export const initGraphql = (app) => {
+export const initGraphql = (app: Router) => {
   app.use('/graphql', authenticate(true), graphqlHTTP((req, res) => ({
     context: { req, res },
     schema: schemas,
--- src/graphql/resolver.ts
+++ src/graphql/resolver.ts
@@ -1,17 +1,24 @@
+import { IxSub } from './../model/sub-model'
+import { getAllThread, applyVoteStatus, getSubFromId } from './../services/thread-service'
 import { IxThread, Thread } from './../model/thread-model'
 import { getUserFromContext } from './util'
 
 export const resolvers = {
   // If function is defined under root...
   // (args, context, info) => {}
-  threads: async (args, context, info) => {
+  threads: async ({ subName }, context, info) => {
     const user = getUserFromContext(context)
-    const threads: IxThread[] = await Thread
-      .find()
-      .populate('Author', [])
-      .populate('SubParent', [])
-      .exec()
-    return threads
+    let sub: IxSub
+
+    if (subName) sub = await getSubFromId(subName)
+    const threadList = await getAllThread(sub)
+    if (user) applyVoteStatus(threadList, user)
+
+    return {
+      _id: sub ? sub._id : '',
+      SubLongName: subName,
+      SubThread: threadList,
+    }
   },
   thread: async ({ id }, context, info) => {
     const thread: IxThread = await Thread
